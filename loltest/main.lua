4fc1a1305f7056aa1beead730af0a0de76c41780
a_m0d <damien.schoof@gmail.com>
2011-09-21 20:22:24 -0400

    A moving hero ...

--- loltest/main.lua
+++ loltest/main.lua
@@ -1,3 +1,28 @@
+-- Most of this code is (currently) copied straight from the Love2D
+-- site as I am just beginning to learn Lua and Love2D
+-- Also from http://www.headchant.com/
+
+function love.load()
+    hero = {}
+    hero.x = 300
+    hero.y = 450
+    hero.speed = 100
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("left") then
+        hero.x = hero.x - hero.speed*dt
+    elseif love.keyboard.isDown("right") then
+        hero.x = hero.x + hero.speed*dt
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello, World", 400, 400)
+    -- draw some ground
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.rectangle("fill", 0, 465, 800, 150)
+
+    -- draw a "hero"
+    love.graphics.setColor(255, 0, 255, 255)
+    love.graphics.rectangle("fill", hero.x, hero.y, 30, 15)
 end
