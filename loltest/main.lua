eedd2be5feb928c56f4c1402d2758bc55d084890
a_m0d <damien.schoof@gmail.com>
2011-09-21 20:27:37 -0400

    7 enemies that move slowly down the screen!

--- loltest/main.lua
+++ loltest/main.lua
@@ -7,6 +7,15 @@ function love.load()
     hero.x = 300
     hero.y = 450
     hero.speed = 100
+    enemies = {}
+    for i=0,7 do
+        enemy = {}
+        enemy.width = 40
+        enemy.height = 20
+        enemy.x = i * (enemy.width + 60) + 100
+        enemy.y = enemy.height + 100
+        table.insert(enemies, enemy)
+    end
 end
 
 function love.update(dt)
@@ -14,6 +23,16 @@ function love.update(dt)
         hero.x = hero.x - hero.speed*dt
     elseif love.keyboard.isDown("right") then
         hero.x = hero.x + hero.speed*dt
+    end
+
+    for i,v in ipairs(enemies) do
+        -- enemies slowly drift down
+        v.y = v.y + dt
+
+        -- check for collision with ground
+        if v.y > 465 then
+            -- DIE!!!
+        end
     end
 end
 
@@ -23,6 +42,12 @@ function love.draw()
     love.graphics.rectangle("fill", 0, 465, 800, 150)
 
     -- draw a "hero"
-    love.graphics.setColor(255, 0, 255, 255)
+    love.graphics.setColor(0, 0, 255, 255)
     love.graphics.rectangle("fill", hero.x, hero.y, 30, 15)
+    
+    -- draw the "enemies"
+    love.graphics.setColor(255, 0, 0, 255)
+    for i,v in ipairs(enemies) do
+        love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
+    end
 end
